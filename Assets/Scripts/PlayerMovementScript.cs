a14b94fcf64f2b86005802e78a99a25f9cb6dba0
DGagnon333 <derick.gag46@hotmail.com>
2021-02-03 09:04:09 -0800

    creation test

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 vitesse = new Vector3(400, 0, 150);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        rb.AddForce(vitesse * Time.deltaTime);
+    }
+}
