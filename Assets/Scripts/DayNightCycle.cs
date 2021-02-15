60b95d459563adc5c21c661480461ffd0fa25d37
NassourNassour <78489569+NassourNassour@users.noreply.github.com>
2021-02-15 12:15:40 -0500

    Day Night Cyle (pt1)
    J'ai commencé le daynight cycle (juste les chiffres)

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField]
+    private float targetDayLength;
+
+    [SerializeField]
+    private float timeOfDay;
+
+    public bool pause = false;
+
+    private int dayNumber;
+
+    private void SetTimeScale()
+    {
+        timeScale = 24 / (targetDayLength / 60) ;
+    }
+
+    private void UpdateTime()
+    {
+        timeOfDay += timeScale / 86400; // seconds in a day
+        if(timeOfDay > 1)
+        {
+            dayNumber++;
+            timeOfDay -= 1;
+            pause = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(!pause)
+        {
+            SetTimeScale();
+            UpdateTime();
+        }
+    }
+}
