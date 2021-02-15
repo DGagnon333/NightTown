96a1c1149b633d6368aaabb0f674e8f09ce1656e
guivarin <201947209@edu.clg.qc.ca>
2021-02-15 12:14:13 -0500

    enemy behaviour

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,22 +1,42 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//// Auteur: Guillaume
+//public class EnemyBehaviour : MonoBehaviour
+//{
+//    [SerializeField]
+//    const int Movement_Speed = 10; // Guillaume: à ajuster
+//    [SerializeField]
+//    int health = 100;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// Auteur: Guillaume
+
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField]
-    const int Movement_Speed = 10; // Guillaume: à ajuster
-    [SerializeField]
-    int health = 100;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject[] joueur;
+    [SerializeField] private string targetTag;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private int moveSpeed = 150;
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 dir = new Vector3();
+    private void Update()
     {
-        
+        joueur = GameObject.FindGameObjectsWithTag(targetTag);
+        dir = joueur[0].transform.position - transform.position;
+        rb.MovePosition(transform.position + dir.normalized * moveSpeed * Time.deltaTime);
     }
 }
+
