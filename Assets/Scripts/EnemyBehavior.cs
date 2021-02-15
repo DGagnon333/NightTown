fdf5ea782fcf8fb091be50858add3a9e8dc1e092
guivarin <201947209@edu.clg.qc.ca>
2021-02-15 11:51:24 -0500

    New scenes and new prefabs player and enemy

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Auteur: Guillaume
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField]
+    const int Movement_Speed = 10; // Guillaume: à ajuster
+    [SerializeField]
+    int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
