873952ec322ab58a356fefddff84503b786b01e7
NassourNassour <78489569+NassourNassour@users.noreply.github.com>
2021-02-17 09:52:40 -0500

    DayNight final version
    gg bois

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -14,12 +14,31 @@ public class DayNightCycle : MonoBehaviour
 
     [SerializeField]
     private float timeOfDay; // Cet attribut est une valeure de 0 à 1 qui établit l'heure courante de la journée.
+    public float TimeOfDay
+    {
+        get => timeOfDay;
+    }
 
     public bool pause = false; // Cet attribut permet de pauser le passage du temps. (ne pause pas le jeu)
+    public bool Pause
+    {
+        get => pause;
+    }
+
 
     private int dayNumber; // Cet attribut permet d'établir le jour courant du joueur.
+    public int DayNumber
+    {
+        get => dayNumber;
+    }
 
     private float timeScale; // Permet de convertir le temps réel en temps virtuel selon la longueure d'une journée.
+
+    private bool isDay;
+    public bool IsDay
+    {
+        get => isDay;
+    }
 
     const int SECONDS_IN_DAY = 84000;
 
@@ -63,11 +82,12 @@ public class DayNightCycle : MonoBehaviour
     private void AdjustSunRotation()
     {
         float sunAngle = (timeOfDay * 180f) - 75f;
-        dailyRotation.transform.localRotation = Quaternion.Euler(0, 0, sunAngle); // bruh? newVector3 missing
-
+        dailyRotation.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, sunAngle)); 
+        // Applique une rotation sur le soleil
     }
 
     ////////////////////////////////////////////////////////////////////////
+    // Partie 3: Intensité du soleil.
 
     [SerializeField]
     private Light sun;
@@ -79,14 +99,8 @@ public class DayNightCycle : MonoBehaviour
         intensity = Vector3.Dot(sun.transform.forward, Vector3.down);
         Mathf.Clamp01(intensity);
 
+        // Projection orthogonale du vecteur du soleil sur l'axe des y.
+
         sun.intensity = intensity;
     }
-
-
-
-
-
-
-
-
 }
