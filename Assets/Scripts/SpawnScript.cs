f8a10a3a251050f95ea9c9d03959bbe7bb2fdb19
guivarin <201947209@edu.clg.qc.ca>
2021-02-17 11:23:31 -0500

    update spawnscript

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿// auteur: Guillaume Varin avec aide de Dérick Gagnon pour la logique du spawn loop
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
@@ -10,20 +11,24 @@ public class SpawnScript : MonoBehaviour
 
     [SerializeField] int spawnTime = 3;
 
-    [SerializeField] private GameObject ennemie;
+    [SerializeField] private GameObject spawnObject;
 
     [SerializeField] private int range;
+    private void Start()
+    {
+        
+    }
     private void Update()
     {
-        SpawnLoop();
+        SpawnLoop(spawnObject, spawnTime);
     }
 
-    private void SpawnLoop()
+    private void SpawnLoop(GameObject spawnObject, int spawnTime)
     {
         time -= Time.deltaTime;
         if (time <= 0)
         {
-            Instantiate(ennemie, new Vector3(Random.Range(-range, range) + transform.position.x,
+            Instantiate(spawnObject, new Vector3(Random.Range(-range, range) + transform.position.x,
                 Random.Range(0, 0) + transform.position.y, Random.Range(-range, range) + transform.position.z), Quaternion.identity);
             time += spawnTime;
         }
