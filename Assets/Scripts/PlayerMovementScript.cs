0f348be8450ff1bd23d64cf480c68d577bdecbf9
MykArsenaut <78366697+MykArsenaut@users.noreply.github.com>
2021-02-17 09:31:59 -0500

    Update Player Movement Script
    PlayerMoveScript

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -6,13 +6,38 @@ using UnityEngine;
 public class PlayerMovementScript : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float walkSpeed = 150f;
+    [SerializeField] float runSpeed =200f;
+    public bool playerIsOnGround = true;
+
     private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        float moveSpeed; 
+        if (Input.GetKey (KeyCode.L))
+        {
+            moveSpeed = runSpeed;
+        }
+
+        else
+        {
+            moveSpeed = walkSpeed;
+        }
 
         Vector3 movement = (transform.right * x + transform.forward * z).normalized;
         rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && playerIsOnGround == true)
+        {
+            rb.AddForce(new Vector3(0, 20, 0), ForceMode.Impulse);
+            playerIsOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            playerIsOnGround = true;
     }
 }
