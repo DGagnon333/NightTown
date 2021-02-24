dd46cb5a56a1411c1921c1e979a2cb509e0dbbd5
guivarin <201947209@edu.clg.qc.ca>
2021-02-24 10:56:41 -0500

    wave manager et spawn script

--- Assets/Scripts/EnemyWaveManager.cs
+++ Assets/Scripts/EnemyWaveManager.cs
@@ -1,29 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class EnemyWaveManager : MonoBehaviour
-{
-    enum WaveState { Inactive, Activation, Attack, Defeated }
+//public class EnemyWaveManager : MonoBehaviour
+//{
+//    enum WaveState { Inactive, Activation, NbWaveStates }
 
-    [SerializeField]
-    GameObject Enemy;
+//    [SerializeField]
+//    GameObject Enemy;
+//    [SerializeField]
+//    GameObject DayNightManager;
+//    private bool waveActivated { get; set; }
+//    private int waveNumber { get; set; }
+//    private bool playerDead { get; set; }
 
-    private bool waveActivated;
-    private WaveState CurrentState { get; set; }
-    private int waveNumber { get; set; }
-    private bool playerDead { get; set; }
-    void Start()
-    {
-        CurrentState = WaveState.Inactive;
-    }
+//    private DayNightCycle dayNightCycle;
+//    private WaveState CurrentState { get; set; }
+    
+//    void Start()
+//    {
+//        CurrentState = WaveState.Inactive;
+//        DayNightCycle dayNightCycle = DayNightManager.GetComponentInChildren<DayNightCycle>();
+//    }
 
-    void Update()
-    {
-        
-    }
-    private void WaveActivation()
-    {
-        
-    }
-}
+//    void Update()
+//    {
+//        bool isDay = dayNightCycle.IsDay;
+//    }
+//    // Guillaume: pas certain du input pour l'activation d'une vague ennemi. 
+//    //            --> hésite entre un bouton (UI) dans la base ou une key sur le clavier
+//    private void WaveActivation(bool isDay)
+//    {
+//        KeyCode waveActivationKey = KeyCode.V; // Guillaume: Input temporaire pour l'activation d'une vague
+//        if (!isDay && Input.GetKeyDown(waveActivationKey))
+//        {
+//            waveActivated = true;
+
+//        }
+//    }
+//}
