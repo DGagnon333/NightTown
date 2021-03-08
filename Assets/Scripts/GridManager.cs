9581094054e8bca162ee697209d9a6b09986e4d5
DGagnon333 <derick.gag46@hotmail.com>
2021-03-08 09:00:01 -0800

    push test pour gitIgnore DG

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -13,11 +13,20 @@ using UnityEditorInternal;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int gridSize = 100;
-    public Vector3[,] array;
     private GameObject[] tiles;
     private int step = 2;
-    private Dictionary<Vector3, bool> tileState;
+    private bool[,] tileState;
+    //public Vector3[,] array;
+    //private Dictionary<Vector3, bool> tileState;
     //private Dictionary<int, Tile> array2;
+
+
+
+
+
+    //avoir un tableau 2d de bool, dans le tableau se souvenir si il y a un cube ou non à l'indice ij. Besoin d'une fnction qui va
+    //prendre des float x et z et la convertir en indice ij     
+
 
     private void Awake()
     {
@@ -31,22 +40,25 @@ public class GridManager : MonoBehaviour
     /// <returns></returns>
     private void ArrayCreation()
     {
-        array = new Vector3[gridSize, gridSize];
-        tileState = new Dictionary<Vector3, bool>(gridSize);
+        //array = new Vector3[gridSize, gridSize];
+        //tileState = new Dictionary<Vector3, bool>(gridSize);
         //int nb = 0;
         //array2 = new Dictionary<int, Tile>(gridSize);
+        tileState = new bool[gridSize, gridSize];
+
         for (int z = 0; z < gridSize; z++)
         {
 
             for (int x = 0; x < gridSize; x++)
             {
+                tileState[x, z] = true;
                 //nb++;
                 //array2[nb] = new Tile(x * step - gridSize, z * step - gridSize);
                 //array2[nb].State = true;
                 //Instantiate(tiles[0], new Vector3(array2[nb].X, 0, array2[nb].Z), Quaternion.identity);
-                array[x, z] = new Vector3(x * step - gridSize, 0, z * step - gridSize);
-                tileState[new Vector3((int)array[x, z].x, 0, (int)array[x, z].z)] = true;
-                Instantiate(tiles[0], array[x, z] , Quaternion.identity);
+                //array[x, z] = new Vector3(x * step - gridSize, 0, z * step - gridSize);
+                //tileState[new Vector3((int)array[x, z].x, 0, (int)array[x, z].z)] = true;
+                Instantiate(tiles[0], new Vector3(x * step - gridSize, 0, z * step - gridSize), Quaternion.identity);
             }
         }
     }
@@ -59,103 +71,145 @@ public class GridManager : MonoBehaviour
     /// <param name = "newBuilding" > le bâtiment choisi</param>
     /// <param name = "tf" > la position du "phantôme" de l'objet sélectionné</param>
     /// <param name = "scale" > la taille de l'objet sélectionné</param>
+    //public void TileState(GameObject newBuilding, Transform tf, Vector3 scale)
+    //{
+    //    /////////////////// Version 1 (plus rapide, mais un peu moins précise) ////////////////////////
+    //    Vector3 scaleDiff = new Vector3(scale.x, 0, scale.z) - (new Vector3(1, 0, 1) * step);
+    //    int tileDispo = 0;
+    //    if (scaleDiff != Vector3.zero) //si l'objet n'est pas de la même taille qu'une case, sa position est modifiée pour qoit la bonne, ainsi que son espace utilisé
+    //    {
+    //        for (int z = 0; z < scaleDiff.z; z++)
+    //        {
+    //            for (int x = 0; x < scaleDiff.x; x++)
+    //            {
+    //                if (!tileState[tf.position - Vector3.one + new Vector3(x * step, 0, z * step)])
+    //                {
+    //                    tileDispo++;
+    //                }
+    //            }
+    //        }
+    //        for (int z = 0; z < scaleDiff.z; z++)
+    //        {
+    //            for (int x = 0; x < scaleDiff.x; x++)
+    //            {
+
+    //                if (tileDispo == 0)
+    //                {
+
+    //                    Instantiate(newBuilding, tf.position, Quaternion.identity);
+    //                    tileState[tf.position - Vector3.one + new Vector3(x * step, 0, z * step)] = false;
+
+    //                }
+    //            }
+    //        }
+
+    //    }
+    //    //if (scaleDiff == Vector3.zero)
+    //    //{
+    //    //    if (tileState[tf.position])
+    //    //    {
+    //    //        Instantiate(newBuilding, tf.position, Quaternion.identity);
+    //    //        tileState[tf.position] = false;
+    //    //    }
+    //    //}
+
+    //    //////////////////// VERSION 2 (version qui prend plus de mémoire mais plus précise) ////////////////////////
+
+    //    ////int tileDispo = 0;
+    //    ////if (scaleDiff != Vector3.zero)
+    //    ////{
+    //    ////        if (tileState[tf.position - Vector3.one])
+    //    ////        {
+    //    ////            Instantiate(newBuilding, tf.position, Quaternion.identity);
+    //    ////            for (int z = 1; 0 < scaleDiff.z; z++)
+    //    ////            {
+    //    ////                for (int x = 1; 0 < scaleDiff.x; x++)
+    //    ////                {
+    //    ////                    tileState[tf.position - Vector3.one + new Vector3(x, 0, z)] = false;
+    //    ////                }
+    //    ////            }
+    //    ////            tileState[tf.position - Vector3.one] = false;
+    //    ////        }
+
+    //    ////}
+    //    if (scaleDiff == Vector3.zero)
+    //    {
+    //        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!correct
+    //        for (int z = 0; z < gridSize; z++)
+    //        {
+    //            for (int x = 0; x < gridSize; x++)
+    //            {
+    //                if (array[x, z] == tf.position && tileState[array[x, z]])
+    //                {
+    //                    Instantiate(newBuilding, tf.position, Quaternion.identity);
+    //                    tileState[array[x, z]] = false;
+
+    //                }
+    //            }
+    //        }
+    //    }
+    //    ////////////////////////////Version3
+    //    //Vector3 scaleDiff = new Vector3(scale.x, 0, scale.z) - (new Vector3(1, 0, 1) * step);
+    //    //int tileDispo = 0;
+    //    //if (scaleDiff == Vector3.zero) //si l'objet n'est pas de la même taille qu'une case, sa position est modifiée pour qoit la bonne, ainsi que son espace utilisé
+    //    //{
+    //    //    for (int z = 0; z < gridSize; z++)
+    //    //    {
+
+    //    //        for (int x = 0; x < gridSize; x++)
+    //    //        {
+    //    //            tileDispo++;
+    //    //            if (array2[tileDispo].State && array2[tileDispo].X == (int)tf.position.x && array2[tileDispo].Z == (int)tf.position.z)
+    //    //            {
+    //    //                Instantiate(newBuilding, tf.position, Quaternion.identity);
+    //    //                array2[tileDispo].State = false;
+    //    //            }
+    //    //        }
+    //    //    }
+
+    //    //}
+    //}
     public void TileState(GameObject newBuilding, Transform tf, Vector3 scale)
     {
-        /////////////////// Version 1 (plus rapide, mais un peu moins précise) ////////////////////////
-        Vector3 scaleDiff = new Vector3(scale.x, 0, scale.z) - (new Vector3(1, 0, 1) * step);
+        int posX = (int)(tf.position.x + gridSize) / 2;
+        int posZ = (int)(tf.position.z + gridSize) / 2;
+        int scaleDiffX = (int)(scale.x -2) / 2;
         int tileDispo = 0;
-        if (scaleDiff != Vector3.zero) //si l'objet n'est pas de la même taille qu'une case, sa position est modifiée pour qoit la bonne, ainsi que son espace utilisé
+
+        if (scaleDiffX != 0)
         {
-            for (int z = 0; z < scaleDiff.z; z++)
+            for (int z = 0; z < scaleDiffX; z++)
             {
-                for (int x = 0; x < scaleDiff.x; x++)
+                for (int x = 0; x < scaleDiffX; x++)
                 {
-                    if (!tileState[tf.position - Vector3.one + new Vector3(x * step, 0, z * step)])
+                    if (!tileState[x + scaleDiffX, z + scaleDiffX])
                     {
+                        Debug.Log(x + scaleDiffX + ", " + z + scaleDiffX + " ... " + tileState[x + scaleDiffX, z + scaleDiffX]);
+
                         tileDispo++;
                     }
                 }
             }
-            for (int z = 0; z < scaleDiff.z; z++)
+            if (tileDispo == 0)
             {
-                for (int x = 0; x < scaleDiff.x; x++)
+                //Instantiate(newBuilding, new Vector3(posX * step - gridSize, 0, posZ * step - gridSize) + new Vector3(1, 1, 1), Quaternion.identity);
+                for (int z = 0; z < scaleDiffX; z++)
                 {
-
-                    if (tileDispo == 0)
+                    for (int x = 0; x < scaleDiffX; x++)
                     {
-
-                        Instantiate(newBuilding, tf.position, Quaternion.identity);
-                        tileState[tf.position - Vector3.one + new Vector3(x * step, 0, z * step)] = false;
+                        tileState[x + scaleDiffX, z + scaleDiffX] = false;
+                        Debug.Log(x + scaleDiffX + ", " + z + scaleDiffX + " ... " + tileState[x + scaleDiffX, z + scaleDiffX]);
 
                     }
                 }
             }
-
         }
-        if (scaleDiff == Vector3.zero)
+
+        if (tileState[posX, posZ])
         {
-            if (tileState[tf.position])
-            {
-                Instantiate(newBuilding, tf.position, Quaternion.identity);
-                tileState[tf.position] = false;
-            }
+            Instantiate(newBuilding, new Vector3(posX * step - gridSize, 0, posZ * step - gridSize), Quaternion.identity);
+            tileState[posX, posZ] = false;
         }
-
-        //////////////////// VERSION 2 (version qui prend plus de mémoire mais plus précise) ////////////////////////
-
-        ////int tileDispo = 0;
-        ////if (scaleDiff != Vector3.zero)
-        ////{
-        ////        if (tileState[tf.position - Vector3.one])
-        ////        {
-        ////            Instantiate(newBuilding, tf.position, Quaternion.identity);
-        ////            for (int z = 1; 0 < scaleDiff.z; z++)
-        ////            {
-        ////                for (int x = 1; 0 < scaleDiff.x; x++)
-        ////                {
-        ////                    tileState[tf.position - Vector3.one + new Vector3(x, 0, z)] = false;
-        ////                }
-        ////            }
-        ////            tileState[tf.position - Vector3.one] = false;
-        ////        }
-
-        ////}
-        //if (scaleDiff == Vector3.zero)
-        //{
-        //    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!correct
-        //    for (int z = 0; z < gridSize; z++)
-        //    {
-        //        for (int x = 0; x < gridSize; x++)
-        //        {
-        //            if (array[x, z] == tf.position && tileState[array[x, z]])
-        //            {
-        //                Instantiate(newBuilding, tf.position, Quaternion.identity);
-        //                tileState[tf.position] = false;
-
-        //            }
-        //        }
-        //    }
-        //}
-        ////////////////////////////Version3
-        //Vector3 scaleDiff = new Vector3(scale.x, 0, scale.z) - (new Vector3(1, 0, 1) * step);
-        //int tileDispo = 0;
-        //if (scaleDiff == Vector3.zero) //si l'objet n'est pas de la même taille qu'une case, sa position est modifiée pour qoit la bonne, ainsi que son espace utilisé
-        //{
-        //    for (int z = 0; z < gridSize; z++)
-        //    {
-
-        //        for (int x = 0; x < gridSize; x++)
-        //        {
-        //            tileDispo++;
-        //            if (array2[tileDispo].State && array2[tileDispo].X == (int)tf.position.x && array2[tileDispo].Z == (int)tf.position.z)
-        //            {
-        //                Instantiate(newBuilding, tf.position, Quaternion.identity);
-        //                array2[tileDispo].State = false;
-        //            }
-        //        }
-        //    }
-
-        //}
     }
 }
 public class Tile
