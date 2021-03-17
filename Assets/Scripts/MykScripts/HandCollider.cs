4651c36beb53f138375ffd92af141fe51648495c
MykArsenaut <78366697+MykArsenaut@users.noreply.github.com>
2021-03-17 09:40:03 -0400

    Script Myk

---
+++ Assets/Scripts/MykScripts/HandCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollider : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+    private void Awake()
+    {
+        boxCollider.material = new PhysicMaterial() { bounciness = 0, };
+    }
+    
+
+    [SerializeField] Rigidbody rb;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Ground")
+            rb.velocity = Vector3.zero;
+    }
+}
