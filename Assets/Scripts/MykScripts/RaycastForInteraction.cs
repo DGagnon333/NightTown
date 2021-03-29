c8abaefd86d84681fb657a5d6fbf26aa0e823142
MykArsenaut <78366697+MykArsenaut@users.noreply.github.com>
2021-03-29 10:11:26 -0400

    Interaction 2.0

--- Assets/Scripts/MykScripts/RaycastForInteraction.cs
+++ Assets/Scripts/MykScripts/RaycastForInteraction.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿//Fait par Mykaël
+//Vidéo ayant permis de comprendre le fondtionnement d'un système d'intéraction : https://www.youtube.com/watch?v=saM9D1V6uNg
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
