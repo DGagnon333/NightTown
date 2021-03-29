1574fc525e95b89185a978bf55cd3fc5b38bfc58
NassourNassour <78489569+NassourNassour@users.noreply.github.com>
2021-03-29 10:03:28 -0400

    pepo

--- Assets/Scripts/NasScripts/NasScript2/TorchComponent.cs
+++ Assets/Scripts/NasScripts/NasScript2/TorchComponent.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class TorchComponent : MonoBehaviour
 {
+    [SerializeField]
+    private bool consumable = false;
+
+    [SerializeField]
+    private float maxRange = 100f; // portée maximale
+
+    [SerializeField]
+    private float minRange = 0.4f; // [0,1] pour le pourcentage d'efficacité selon la portée maximale
+
     private Light torch;
 
     private void Awake()
     {
         torch = GetComponentInChildren<Light>();
+        torch.range = maxRange;
     }
 
     private void Start()
     {
-        
+        if(consumable)
+        {
+            InvokeRepeating("reduceLightRange", 0f, 5f);
+        }       
+    }
+
+    private void reduceLightRange()
+    {
+        torch.range -= 2;
     }
 }
