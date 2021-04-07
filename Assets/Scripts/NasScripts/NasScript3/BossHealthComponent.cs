e473573d823c5aea922b36fbad7d9d7af1710a90
NassourNassour <78489569+NassourNassour@users.noreply.github.com>
2021-04-07 11:01:20 -0400

    bosshealth

---
+++ Assets/Scripts/NasScripts/NasScript3/BossHealthComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthComponent : MonoBehaviour
+{
+    public float maxHealth = 300f;
+    public float currentHealth;
+    public HealthBarComponent healthBar; 
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Start()
+    {
+        healthBar.SetMaxHealth((int)maxHealth);
+    }
+
+    public void TakeDamage(float damageTaken)
+    {
+        currentHealth -= damageTaken;
+        healthBar.SetHealth((int)currentHealth);
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject, 1);
+            Debug.Log("less goo");
+        }
+    }
+}
