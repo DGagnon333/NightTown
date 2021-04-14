84d60f3b9c94c3284ed3fd07e93f43b07a774bce
DGagnon333 <derick.gag46@hotmail.com>
2021-04-14 07:11:18 -0700

    OutOfBounds

---
+++ Assets/Scripts/DG/OutOfBounds.cs
@@ -0,0 +1,35 @@
+﻿//Cette classe s'occupe de voir si le joueur est en train de tomber dans le vide. 
+//Si C'est le cas, sa position retournera à celle du début de la partie.
+
+//Fait par Dérick
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    float time = 0;
+    private Vector3 position;
+    private void Awake()
+    {
+        position = transform.position;
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 4)
+        {
+            time = 0;
+            OfBounds();
+        }
+    }
+
+    private void OfBounds()
+    {
+        if (transform.position.y <= -5)
+        {
+            transform.position = position;
+        }
+    }
+}
