65847f91b77a07560bd51fb03a5823c5fdcaf1b4
MykArsenaut <78366697+MykArsenaut@users.noreply.github.com>
2021-04-19 10:19:07 -0400

    Maps

---
+++ Assets/Scripts/MykScripts/ShowBigMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBigMap : MonoBehaviour
+{
+
+    public GameObject map;
+
+    void Start()
+    {
+        map.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            map.SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            map.SetActive(false);
+        }
+    }
+}
