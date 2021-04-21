9479f4f5371c444a2e96288e3bf731427c70fb6b
guivarin <201947209@edu.clg.qc.ca>
2021-04-21 10:56:11 -0400

    WeaponShop Update

---
+++ Assets/Scripts/GuigScripts/IWeaponShopClient.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWeaponShopClient
+{
+    void BoughtItem(PlayerItem.PlayerItemType itemType);
+}
