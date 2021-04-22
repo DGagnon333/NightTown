4713510a922f4b209d4edbb23ef4e4d79dae44e5
DGagnon333 <derick.gag46@hotmail.com>
2021-04-21 19:31:19 -0700

    Test scene caracter animation

---
+++ Assets/Scripts/DG/animatorScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatorScript : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("IsWalking");
+
+    }
+    void Update()
+    {
+        //début des animations
+        bool isWalking = animator.GetBool(isWalkingHash);
+
+        if (!isWalking)
+        {
+            if (Input.GetKey("w"))
+            {
+                animator.SetBool(isWalkingHash, true);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            if (Input.GetKey("s"))
+            {
+                animator.SetBool(isWalkingHash, true);
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            if (Input.GetKey("a"))
+            {
+                animator.SetBool(isWalkingHash, true);
+                transform.localRotation = Quaternion.Euler(0, -90, 0);
+            }
+            if (Input.GetKey("d"))
+            {
+                animator.SetBool(isWalkingHash, true);
+                transform.localRotation = Quaternion.Euler(0, 90, 0);
+            }
+        }
+        if (isWalking && !(Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("a") || Input.GetKey("d")))
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+        //if (Input.GetButtonDown("Jump") && playerIsOnGround == true)
+        //{
+        //    rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+        //    playerIsOnGround = false;
+        //    animator.SetBool(isWalkingHash, true);
+
+        //}
+
+        //les scripts d'animations ont été fortement inspiré par Nicky B, 
+        //sur youtube, How to Animate Characters in Unity 3D | Animation Transitions With Booleans
+        //fin animations
+    }
+}
--- Assets/Scripts/MykScripts/PlayerMovementScript.cs
+++ Assets/Scripts/MykScripts/PlayerMovementScript.cs
@@ -10,16 +10,12 @@ public class PlayerMovementScript : MonoBehaviour
     [SerializeField] float walkSpeed = 50f;
     [SerializeField] float runSpeed = 100f;
     public bool playerIsOnGround = true;
-    Animator animator;
-    int isWalkingHash;
 
     private Rigidbody body;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
-        isWalkingHash = Animator.StringToHash("IsWalking");
     }
 
     private void Start()
@@ -29,46 +25,29 @@ public class PlayerMovementScript : MonoBehaviour
 
     private void Update()
     {
-        //début des animations
-        bool isWalking = animator.GetBool(isWalkingHash);
-        bool forwardPressed = Input.GetKey("w");
-        if (!isWalking && forwardPressed)
-        {
-            animator.SetBool(isWalkingHash, true);
-        }
-        if (isWalking && !forwardPressed)
-        {
-            animator.SetBool(isWalkingHash, false);
-        }
-        //les scripts d'animations ont été fortement inspiré par Nicky B, 
-        //sur youtube, How to Animate Characters in Unity 3D | Animation Transitions With Booleans
-        //fin animations
+        
 
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
-        float moveSpeed;
-        if (Input.GetKey(KeyCode.L))
-        {
-            moveSpeed = runSpeed;
-        }
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            float moveSpeed;
+            if (Input.GetKey(KeyCode.L))
+            {
+                moveSpeed = runSpeed;
+            }
 
-        else
-        {
-            moveSpeed = walkSpeed;
-        }
+            else
+            {
+                moveSpeed = walkSpeed;
+            }
 
-        Vector3 movement = (transform.right * x + transform.forward * z).normalized;
-        rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+            Vector3 movement = (transform.right * x + transform.forward * z).normalized;
+            rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump") && playerIsOnGround == true)
-        {
-            rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
-            playerIsOnGround = false;
-        }
-        rb.constraints = RigidbodyConstraints.FreezeRotationX;
-        rb.constraints = RigidbodyConstraints.FreezeRotationZ;
+            
+            rb.constraints = RigidbodyConstraints.FreezeRotationX;
+            rb.constraints = RigidbodyConstraints.FreezeRotationZ;
+        
     }
-
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
