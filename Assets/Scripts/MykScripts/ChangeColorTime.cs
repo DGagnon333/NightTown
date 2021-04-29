d21cf60b9deb28523670898ae0f0bff320714cc3
MykArsenaut <78366697+MykArsenaut@users.noreply.github.com>
2021-04-28 22:36:38 -0400

    Change color time

---
+++ Assets/Scripts/MykScripts/ChangeColorTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorTime : MonoBehaviour
+{
+    float theTime = 3.0f;
+    Color color1 = Color.green;
+    Color color2 = Color.red;
+
+    private Light light;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        // set light color
+        float t = Mathf.PingPong(Time.time, theTime) / theTime;
+        light.color = Color.Lerp(color1, color2, t);
+    }
+}
