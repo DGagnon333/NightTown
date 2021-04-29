f348085a57e468a7dd95a4ae5cea974d5cfb25d5
MykArsenaut <78366697+MykArsenaut@users.noreply.github.com>
2021-04-28 21:29:44 -0400

    Changement Pathfinding

--- Assets/Scripts/MykScripts/Pathfinding.cs
+++ Assets/Scripts/MykScripts/Pathfinding.cs
@@ -5,65 +5,65 @@ using UnityEngine;
 public class Pathfinding : MonoBehaviour
 {
 
-    Grid GridReference;//For referencing the grid class
-    public Transform StartPosition;//Starting position to pathfind from
-    public Transform TargetPosition;//Starting position to pathfind to
+    Grid theGrid;//Permet d'acceder au Script/classe "Grid"
+    public Transform StartPosition;//Position du départ pour le pathfinding
+    public Transform TargetPosition;//Position d'arrivé pour le pathfinding
 
-    private void Awake()//When the program starts
+    private void Awake()
     {
-        GridReference = GetComponent<Grid>();//Get a reference to the game manager
+        theGrid = GetComponent<Grid>();//Obtenir une référence au Grid (une matrice) pour pouvoir faire le pathfinding.
     }
 
-    private void Update()//Every frame
+    private void Update()
     {
-        FindPath(StartPosition.position, TargetPosition.position);//Find a path to the goal
+        FindPath(StartPosition.position, TargetPosition.position);//Une fonction qui trouve le chemin le plus court.
     }
 
-    void FindPath(Vector3 a_StartPos, Vector3 a_TargetPos)
+    void FindPath(Vector3 StartPosition, Vector3 FinalPosition)
     {
-        Node StartNode = GridReference.NodeFromWorldPoint(a_StartPos);//Gets the node closest to the starting position
-        Node TargetNode = GridReference.NodeFromWorldPoint(a_TargetPos);//Gets the node closest to the target position
+        Node StartNode = theGrid.NodeFromWorldPoint(StartPosition);//Trouve le noeud où se trouve la position initale
+        Node FinalNode = theGrid.NodeFromWorldPoint(FinalPosition);//Trouve le noeud où se trouve à la position finale. 
 
-        List<Node> OpenList = new List<Node>();//List of nodes for the open list
-        HashSet<Node> ClosedList = new HashSet<Node>();//Hashset of nodes for the closed list
+        List<Node> OpenList = new List<Node>();//Liste de noeuds pour faire le tour des noeuds.
+        HashSet<Node> ClosedList = new HashSet<Node>();//Autre liste de noeuds (avec l'utilisation de HashSet qui est un peu différent de "List" pour conserver les noeuds utilisés dans le chemin.
 
-        OpenList.Add(StartNode);//Add the starting node to the open list to begin the program
+        OpenList.Add(StartNode);//Ajoute le noeud de départ dans la liste
 
-        while (OpenList.Count > 0)//Whilst there is something in the open list
+        while (OpenList.Count > 0)//Tant que la liste n'est pas vide, on fait certaines actions.
         {
-            Node CurrentNode = OpenList[0];//Create a node and set it to the first item in the open list
-            for (int i = 1; i < OpenList.Count; i++)//Loop through the open list starting from the second object
+            Node CurrentNode = OpenList[0];//Créer un noeud et le mettre comme premier noeud de la liste.
+            for (int i = 1; i < OpenList.Count; i++)//Boucle passant au travers les noeuds de la liste. 
             {
-                if (OpenList[i].FCost < CurrentNode.FCost || OpenList[i].FCost == CurrentNode.FCost && OpenList[i].ihCost < CurrentNode.ihCost)//If the f cost of that object is less than or equal to the f cost of the current node
+                if (OpenList[i].fCost < CurrentNode.fCost || OpenList[i].fCost == CurrentNode.fCost && OpenList[i].hCost < CurrentNode.hCost)//Si le cout total d'un noeud de la liste est plus petit ou égal à celui qui est le noeud retenu présentement. 
                 {
-                    CurrentNode = OpenList[i];//Set the current node to that object
+                    CurrentNode = OpenList[i];//Mettre ce noeud comme étant le noeud présent.
                 }
             }
-            OpenList.Remove(CurrentNode);//Remove that from the open list
-            ClosedList.Add(CurrentNode);//And add it to the closed list
+            OpenList.Remove(CurrentNode);//Enlever ce noeud de la première liste.
+            ClosedList.Add(CurrentNode);//Rajouter le noeud dans la liste utilisée pour conserver les noeuds
 
-            if (CurrentNode == TargetNode)//If the current node is the same as the target node
+            if (CurrentNode == FinalNode)//Si le noeud présent est la destination finale.
             {
-                GetFinalPath(StartNode, TargetNode);//Calculate the final path
+                GetFinalPath(StartNode, FinalNode);//Trouve le chemin final
             }
 
-            foreach (Node NeighborNode in GridReference.GetNeighboringNodes(CurrentNode))//Loop through each neighbor of the current node
+            foreach (Node NeighborNode in theGrid.GetNeighboringNodes(CurrentNode))//Boucle qui regarde les noeuds autour du noeud qui est le noeud présent.
             {
-                if (!NeighborNode.bIsWall || ClosedList.Contains(NeighborNode))//If the neighbor is a wall or has already been checked
+                if (!NeighborNode.buildingIsOnNode || ClosedList.Contains(NeighborNode))//Si le noeud est sur un bâtiment ou s'il a déjà été vérifié.
                 {
-                    continue;//Skip it
+                    continue;
                 }
-                int MoveCost = CurrentNode.igCost + GetManhattenDistance(CurrentNode, NeighborNode);//Get the F cost of that neighbor
+                int MoveCost = CurrentNode.gCost + GetManhattenDistance(CurrentNode, NeighborNode);//Trouver le coût total du noeud
 
-                if (MoveCost < NeighborNode.igCost || !OpenList.Contains(NeighborNode))//If the f cost is greater than the g cost or it is not in the open list
+                if (MoveCost < NeighborNode.gCost || !OpenList.Contains(NeighborNode))//Si le cout total est plus petit que le cout pour passer au prochain noeud ou que ce n'est pas dans la liste de noeuds. 
                 {
-                    NeighborNode.igCost = MoveCost;//Set the g cost to the f cost
-                    NeighborNode.ihCost = GetManhattenDistance(NeighborNode, TargetNode);//Set the h cost
-                    NeighborNode.ParentNode = CurrentNode;//Set the parent of the node for retracing steps
+                    NeighborNode.gCost = MoveCost;////Déterminer le coût g du noeud.
+                    NeighborNode.hCost = GetManhattenDistance(NeighborNode, FinalNode);//Déterminer le coût h du noeud.
+                    NeighborNode.ParentNode = CurrentNode;//Mettre le noeud présent comme étant le parent afin de pouvoir retracer le chemin parcouru.
 
-                    if (!OpenList.Contains(NeighborNode))//If the neighbor is not in the openlist
+                    if (!OpenList.Contains(NeighborNode))//Si le noeud n'est pas dans la liste le rajouter
                     {
-                        OpenList.Add(NeighborNode);//Add it to the list
+                        OpenList.Add(NeighborNode);
                     }
                 }
             }
@@ -73,28 +73,28 @@ public class Pathfinding : MonoBehaviour
 
 
 
-    void GetFinalPath(Node a_StartingNode, Node a_EndNode)
+    void GetFinalPath(Node firstNode, Node lastNode)
     {
-        List<Node> FinalPath = new List<Node>();//List to hold the path sequentially 
-        Node CurrentNode = a_EndNode;//Node to store the current node being checked
+        List<Node> FinalPath = new List<Node>();//Liste avec les noeuds du chemin final
+        Node CurrentNode = lastNode;//Mettre le noeud présent comme le dernier noeud.
 
-        while (CurrentNode != a_StartingNode)//While loop to work through each node going through the parents to the beginning of the path
+        while (CurrentNode != firstNode)//Va au travers des parents jusqu'au premier noeud du chemin.
         {
-            FinalPath.Add(CurrentNode);//Add that node to the final path
-            CurrentNode = CurrentNode.ParentNode;//Move onto its parent node
+            FinalPath.Add(CurrentNode);//Ajouter le noeud
+            CurrentNode = CurrentNode.ParentNode;//Mettre le noeud parent comme étant le noeud présent.
         }
 
-        FinalPath.Reverse();//Reverse the path to get the correct order
+        FinalPath.Reverse();//Inverser le chemin pour avoir le bon ordre
 
-        GridReference.FinalPath = FinalPath;//Set the final path
+        theGrid.FinalPath = FinalPath;//Mettre le chemin final.
 
     }
 
-    int GetManhattenDistance(Node a_nodeA, Node a_nodeB)
+    int GetManhattenDistance(Node firstNode, Node secondNode) //Trouver la distance de Manhatten entre deux noeuds.
     {
-        int ix = Mathf.Abs(a_nodeA.iGridX - a_nodeB.iGridX);//x1-x2
-        int iy = Mathf.Abs(a_nodeA.iGridY - a_nodeB.iGridY);//y1-y2
+        int x = Mathf.Abs(firstNode.positionInX - secondNode.positionInX);//x1-x2
+        int y = Mathf.Abs(firstNode.positionInY - secondNode.positionInY);//y1-y2
 
-        return ix + iy;//Return the sum
+        return x + y;
     }
 }
