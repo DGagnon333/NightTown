21f945c1316bc8108cc3b0816d840da2d47b85e2
DGagnon333 <derick.gag46@hotmail.com>
2021-05-02 23:00:30 -0700

    animation dans sample scene

--- Assets/AnimationScriptController.cs
+++ Assets/AnimationScriptController.cs
@@ -24,13 +24,28 @@ public class AnimationScriptController : MonoBehaviour
         bool isWalking = animator.GetBool(isWalkingHash);
         bool isJumping = animator.GetBool(isJumpingHash);
         bool isAttacking = animator.GetBool(isAttackingHash);
-        bool forwardPressed = Input.GetKey("w");
+        bool forwardPressed = (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"));
         bool runPressed = Input.GetKey("left shift");
         bool jumpPressed = Input.GetKey("space");
         bool attackPressed = Input.GetKey("f");
 
 
-
+        if (Input.GetKey("w"))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
         if (!isWalking && forwardPressed)
         {
             animator.SetBool(isWalkingHash, true);
